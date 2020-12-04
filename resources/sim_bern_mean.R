b4c7e975e101b62b3bc85af1cf0e226f87f462e7
Jeff Goldsmith <jeff.goldsmith@columbia.edu>
2020-12-04 17:00:04 -0500

    2020 updates!!

--- resources/sim_bern_mean.R
+++ resources/sim_bern_mean.R
@@ -1,10 +1,9 @@
 sim_bern_mean = function(n, prob) {
   
   sim_data = tibble(
-    y = rbinom(n_samp, 1, prob)
+    x = rbinom(n, 1, prob)
   )
   
-  tibble(
-    samp_avg = mean(sim_data %>% pull(y))
-  )
+  sim_data %>% 
+    summarize(p_hat = mean(x))
 }
